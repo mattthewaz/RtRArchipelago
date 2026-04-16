c9d4c57bb0fab1a6890a47dc5e541c4337c21894
MatthewCole Waschezyn <mattthewaz@gmail.com>
2026-04-16 14:27:27 -0400

    Updated version to 0.1

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 mkdir -p "$(pwd)/../../output/mods"
 OUT="$(realpath "$(pwd)/../../output/mods")"
-cp target/Archipelago-1.0-SNAPSHOT.jar "$OUT/"
+cp target/Archipelago-0.1.jar "$OUT/"
 echo "[Archipelago] Deployed to $OUT"
